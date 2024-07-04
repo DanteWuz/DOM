67f30525b6387105f0bee8a4b89db37e6ad8aa5b
Diogo <164107271+DanteWuz@users.noreply.github.com>
2024-07-03 23:00:59 -0300

    Add files via upload

---
+++ Dom/Estilizacao/script.js
@@ -0,0 +1,8 @@
+function aplicarEstilo()
+{
+    elementoParaEstilizar.style.backgroundColor = "red";
+    elementoParaEstilizar.style.cor = "blue";
+    elementoParaEstilizar.style.fontWeight = "Bold";
+    elementoParaEstilizar.style.borderRadius = "10";
+
+}
